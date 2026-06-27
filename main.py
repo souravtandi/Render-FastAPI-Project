01bfe9afc8af8bcef1866a4f76e2442fccfee0e4
souravtandi <souravtandi10@gmail.com>
2026-06-27 16:15:27 +0530

    feat: corse error fixed

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow frontend origins
+origins = [
+    "http://localhost:3000",   # React (CRA)
+    "http://localhost:5173",   # React (Vite)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   # or ["*"] for testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
     return{
-        "messsage": "Hello from FastAPI"
+        "message": "Hello from FastAPI"
     }
